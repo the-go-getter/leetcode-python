91807be2a623428b8f3c834f60d293a02061be13
Vinay <vinay1020287@gmail.com>
2021-07-28 21:36:14 -0400

    Best time to buy and sell a stock ii

---
+++ 122.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit += prices[i + 1] - prices[i]
+
+        return profit
+
+if __name__ == '__main__':
+    mySol = Solution
+    print(mySol.maxProfit(mySol, [4,1,2,1,2,5]))
